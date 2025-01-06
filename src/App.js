76f7947f0fb203b3b2251787552e2cc9ac1578bc
ren-418 <mrsteevtvw418@gmail.com>
2025-01-07 04:10:25 +0800

    fix issue

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import './App.css';
-import Header from './Header';
-import Home from './Home';
+import "./App.css";
+import Header from "./Header";
+import Home from "./Home";
 import ContactForm from "./ContactForm";
 import Footer from "./Footer";
 
@@ -9,16 +9,33 @@ function App() {
     <div>
       <div className="App">
         <Header />
-        <div style={{color:"white", marginLeft:"50px", marginBottom: "150px"}}>
-          <h1 style={{whiteSpace:"pre-wrap"}}>Energy. <br />Solutions. <br /> Simplified.</h1>
+        <div className="home-contents">
+          <div
+            style={{
+              color: "white"
+            }}
+          >
+            <h1 style={{ whiteSpace: "pre-wrap", fontSize:'76px' }}>
+              Energy. <br />
+              Solutions. <br />Simplified.
+            </h1>
+          </div>
+          <div style={{
+              color: "white",
+              fontSize:'25px'
+            }}>
+            Navigating your site’s energy use is complicated. We keep it simple
+            for you and bring solutions into view.
+          </div>
+          <button className="btn btn-outline-secondary mt-4" style={{
+              color: "white"
+            }}>LEARN MORE</button>
         </div>
       </div>
       <Home />
       <ContactForm />
       <Footer />
     </div>
-    
-    
   );
 }
 
