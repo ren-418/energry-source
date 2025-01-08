c74335e1fa51b3a4d95a9798f0751290a640fed3
Kian314-blip <kianarnold878@gmail.com>
2025-01-08 10:51:40 -0800

    fix

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import Link from "next/link";
+
 
 
 export default function footer() {
@@ -13,27 +15,27 @@ export default function footer() {
                         <div>Follow us.</div>
                     </div>
                     <div className="flex flex-row gap-[10px]">
-                        <a href="">
+                        <Link href="">
                             <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="32" height="32" viewBox="0 0 30 30">
                                 <path d="M15,3C8.373,3,3,8.373,3,15c0,6.627,5.373,12,12,12s12-5.373,12-12C27,8.373,21.627,3,15,3z M10.496,8.403 c0.842,0,1.403,0.561,1.403,1.309c0,0.748-0.561,1.309-1.496,1.309C9.561,11.022,9,10.46,9,9.712C9,8.964,9.561,8.403,10.496,8.403z M12,20H9v-8h3V20z M22,20h-2.824v-4.372c0-1.209-0.753-1.488-1.035-1.488s-1.224,0.186-1.224,1.488c0,0.186,0,4.372,0,4.372H14v-8 h2.918v1.116C17.294,12.465,18.047,12,19.459,12C20.871,12,22,13.116,22,15.628V20z"></path>
                             </svg>
-                        </a>
-                        <a>
+                        </Link>
+                        <Link href="">
                             <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="32" height="32" viewBox="0 0 50 50">
                                 <path d="M25,3C12.85,3,3,12.85,3,25c0,11.03,8.125,20.137,18.712,21.728V30.831h-5.443v-5.783h5.443v-3.848 c0-6.371,3.104-9.168,8.399-9.168c2.536,0,3.877,0.188,4.512,0.274v5.048h-3.612c-2.248,0-3.033,2.131-3.033,4.533v3.161h6.588 l-0.894,5.783h-5.694v15.944C38.716,45.318,47,36.137,47,25C47,12.85,37.15,3,25,3z"></path>
                             </svg>
-                        </a>
-                        <a>
+                        </Link>
+                        <Link href="">
                             <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="32" height="32" viewBox="0 0 50 50">
                                 <path d="M 16 3 C 8.83 3 3 8.83 3 16 L 3 34 C 3 41.17 8.83 47 16 47 L 34 47 C 41.17 47 47 41.17 47 34 L 47 16 C 47 8.83 41.17 3 34 3 L 16 3 z M 37 11 C 38.1 11 39 11.9 39 13 C 39 14.1 38.1 15 37 15 C 35.9 15 35 14.1 35 13 C 35 11.9 35.9 11 37 11 z M 25 14 C 31.07 14 36 18.93 36 25 C 36 31.07 31.07 36 25 36 C 18.93 36 14 31.07 14 25 C 14 18.93 18.93 14 25 14 z M 25 16 C 20.04 16 16 20.04 16 25 C 16 29.96 20.04 34 25 34 C 29.96 34 34 29.96 34 25 C 34 20.04 29.96 16 25 16 z"></path>
                             </svg>
-                        </a>
+                        </Link>
                     </div>
 
                 </div>
                     <div className="flex flex-col text-poppins text-black items-end max-[473px]:items-center">
-                        <a className="underline max-[473px]:text-center"> 888-296-0005 </a>
-                        <a className="underline"> save@millstreetenergy.com</a>
+                        <Link href="" className="underline max-[473px]:text-center"> 888-296-0005 </Link>
+                        <Link href="" className="underline"> save@millstreetenergy.com</Link>
                     </div>
             </div>
         </footer>
